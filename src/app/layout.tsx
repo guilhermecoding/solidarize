dedcdbe4787e673997039cf0dd9a9765949c8528
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-08-31 19:30:12 -0300

    :sparkles: Adiciona configurações do VSCode e atualiza fontes no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Young_Serif, DM_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const youngSerif = Young_Serif({
   subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-young-serif"
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const dmSans = DM_Sans({
   subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-dm-sans"
 });
 
 export const metadata: Metadata = {
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="pt-BR"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+      className={cn("h-full", "antialiased", youngSerif.variable, dmSans.variable, "font-sans")}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
