be674b48c0b57c0a48688c51979ce7ab62932f2a
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-09-20 23:35:50 -0300

    :sparkles: Atualiza o componente SystemLayout para aceitar children como props, simplificando a estrutura do layout e removendo elementos desnecessários.

--- src/app/(sistema)/layout.tsx
+++ src/app/(sistema)/layout.tsx
@@ -9,7 +9,7 @@ import { BreadcrumbItem } from "@/components/ui/breadcrumb";
 import { BreadcrumbLink } from "@/components/ui/breadcrumb";
 import { BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 
-export default function SystemLayout() {
+export default function SystemLayout({ children }: LayoutProps<"/">) {
     return (
         <SidebarProvider>
             <AppSidebar />
@@ -36,14 +36,7 @@ export default function SystemLayout() {
                         </Breadcrumb>
                     </div>
                 </header>
-                <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-                    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                        <div className="aspect-video rounded-xl bg-muted/50" />
-                        <div className="aspect-video rounded-xl bg-muted/50" />
-                        <div className="aspect-video rounded-xl bg-muted/50" />
-                    </div>
-                    <div className="min-h-screen flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-                </div>
+                {children}
             </SidebarInset>
         </SidebarProvider>
     );
